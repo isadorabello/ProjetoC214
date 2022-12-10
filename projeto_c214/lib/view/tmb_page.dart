becc8f14237582674b520f904d8e1e3a903a2556
isadorabello <isadora.bello@gec.inatel.br>
2022-12-10 19:02:59 -0300

    feat: building tmb page

--- projeto_c214/lib/view/tmb_page.dart
+++ projeto_c214/lib/view/tmb_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_c214/controller/tmb.dart';
 
 import '../controller/menu.dart';
 
@@ -10,6 +11,9 @@ class TMBPage extends StatefulWidget {
 }
 
 class _TMBPageState extends State<TMBPage> {
+  int resultado = 0;
+  TMB tmb = TMB();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +21,26 @@ class _TMBPageState extends State<TMBPage> {
         title: const Text('TMB'),
       ),
       drawer: Menu(),
+      body: SizedBox(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.00),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text('TMB: ', style: TextStyle(fontSize: 15)),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Text("$resultado", style: const TextStyle(fontSize: 15)),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
