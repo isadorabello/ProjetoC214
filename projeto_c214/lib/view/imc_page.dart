77abd7ee960088735cba107464fff18db09c627d
isadorabello <isadora.bello@gec.inatel.br>
2022-12-10 11:09:09 -0300

    feat: adding imc and tmb pages

---
+++ projeto_c214/lib/view/imc_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IMCPage extends StatefulWidget {
+  const IMCPage({Key? key}) : super(key: key);
+
+  @override
+  State<IMCPage> createState() => _IMCPageState();
+}
+
+class _IMCPageState extends State<IMCPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ projeto_c214/lib/view/tmb_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TMBPage extends StatefulWidget {
+  const TMBPage({Key? key}) : super(key: key);
+
+  @override
+  State<TMBPage> createState() => _TMBPageState();
+}
+
+class _TMBPageState extends State<TMBPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
