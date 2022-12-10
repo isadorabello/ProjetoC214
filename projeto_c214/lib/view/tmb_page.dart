20131ac0af86fec9c255ffa8c64b34a234a5f12c
isadorabello <isadora.bello@gec.inatel.br>
2022-12-10 18:14:37 -0300

    feat: tmb page

--- projeto_c214/lib/view/tmb_page.dart
+++ projeto_c214/lib/view/tmb_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../controller/menu.dart';
+
 class TMBPage extends StatefulWidget {
   const TMBPage({Key? key}) : super(key: key);
 
@@ -10,6 +12,11 @@ class TMBPage extends StatefulWidget {
 class _TMBPageState extends State<TMBPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TMB'),
+      ),
+      drawer: Menu(),
+    );
   }
 }
